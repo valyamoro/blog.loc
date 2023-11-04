ac5909041766f7729700db832f769f2af3e07432
kutlumbek <Moralek228@gmail.com>
2023-11-04 20:05:11 +0300

    registry fix (Рефакторинг, перенес хеширование пароля и убрал комментарии

--- src/controllers/registry_controller.php
+++ src/controllers/registry_controller.php
@@ -1,56 +1,44 @@
 <?php
-// 27.10.2023 - Рефакторинг: 43 +
 
-// Заголовок страницы.
 $metaTitle = 'Регистрация';
 
-// Пользователь нажал на кнопку регистрации:
 if ($_POST['registry'] === '1') {
-    // Присваиваю введенные пользователем данные.
     $user = $_POST;
-    // Собираю ошибки валидации.
     $errorMessage = validateUser($user);
+
     if (!\is_null($errorMessage)) {
-        // Помещаем в сессию все ошибки валидации, если есть.
         $_SESSION['errors'] = $errorMessage;
     } else {
-        // Маршрут до страницы регистрации.
-        $route = 'auth/registry';
-        // Экранирую данные.
+        $route = '/auth/registry';
+
         $user = escapeData($user);
-        // true, если введенные пользователем пароли совпадают.
         $isPasswordsMatch = isPasswordsMatch($user['password'], $user['confirm_password']);
 
-        // Если введенные пользователем пароли не совпали.
         if ($isPasswordsMatch === false) {
             $_SESSION['errors'] = 'Пароли не совпадают' . "\n";
         } else {
-            // true, если введенная пользователем почта уже существует, иначе false.
             $isUserEmailExists = isUserEmailExists($user['email']);
 
-            // Если есть такая почта:
             if ($isUserEmailExists === true) {
                 $_SESSION['errors'] = 'Пользователь с этими данными уже существует!' . "\n";
             } else {
-                // Создаем пользователя и получаем его айди.
+                $user['password'] = \password_hash($user['password'], PASSWORD_DEFAULT);
                 $lastId = addUser($user);
 
-                // Если при создании пользователя произошла ошибка.
                 if ($lastId === 0) {
                     $_SESSION['errors'] = 'Произошла непредвиденная ошибка!' . "\n";
                 } else {
-                    // Добавляем данные пользователя в сессию.
-                    $_SESSION['notification']['email'] = $user['email'];
-                    $_SESSION['notification']['password'] = $user['password'];
-
-                    // Маршрут до страницы с уведомлением об успешной регистрации.
+                    $_SESSION['success'] = [
+                        'email' => $user['email'],
+                        'password' => $user['password'],
+                    ];
                     $route = '/auth/notification';
                 }
             }
         }
         header("Location: {$route}");
+        die;
     }
 }
 
-// Путь до страницы с регистрацией.
 $content = render($currentAction['view']);
--- src/models/registry_model.php
+++ src/models/registry_model.php
@@ -1,14 +1,11 @@
 <?php
 
 /** Валидация данных пользователя.
- * @param array $data
- * @return null|string
  */
 function validateUser(array $data): ?string
 {
     $result = null;
 
-    // Валидация почты.
     if (empty($data['email'])) {
         $result .= 'Заполните поле почты' . "\n";
     } elseif (!\filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
@@ -19,7 +16,6 @@ function validateUser(array $data): ?string
         $result .= 'Почта содержит недопустимые символы' . "\n";
     }
 
-    // Валидация пароля.
     if (empty($data['password'])) {
         $result .= 'Заполните поле пароль' . "\n";
     } elseif (\is_numeric($data['password'])) {
@@ -32,7 +28,6 @@ function validateUser(array $data): ?string
         $result .= 'Пароль содержит больше 15 символов' . "\n";
     }
 
-    // Валидация имени пользователя.
     if (empty($data['user_name'])) {
         $result .= 'Заполните поле имя' . "\n";
     } elseif (\preg_match('#[^а-яa-z]#ui', $data['user_name'])) {
@@ -43,80 +38,61 @@ function validateUser(array $data): ?string
         $result .= 'Имя содержит менее 4 символов' . "\n";
     }
 
-    // Возвращаю строку с ошибками валидации.
     return $result;
 }
 
 /** Экранирование данных.
- * @param array $data
- * @return array
  */
 function escapeData(array $data): array
 {
     $result = [];
 
     foreach ($data as $key => $value) {
-        // Экранирую и преобразую приходящие данные с помощью специальных функций.
         $result[$key] = \htmlspecialchars(\strip_tags(\trim($value)));
     }
-    // Возвращают массив экранированных данных.
+
     return $result;
 }
 
 /** Проверяем существует ли приходящая почта от пользователя.
- * @param string $email
- * @return bool
  */
 function isUserEmailExists(string $email): bool
 {
-    // Получаем почту.
     $query = 'SELECT email FROM users WHERE email=? LIMIT 1';
 
-    // Подготавливаем запрос на выполнение.
     $sth = connectionDB()->prepare($query);
-    // Запускаем подготовленный запрос на выполнение.
     $sth->execute([$email]);
-    // true, если введенная пользователем почта совпала с существующей.
-    return (bool) $sth->rowCount();
+
+    return (bool)$sth->rowCount();
 }
 
 /** Добавляем нового пользователя в базу данных.
- * @param array $data
- * @return int
  */
 function addUser(array $data): int
 {
-    // Добавляем нового пользователя.
     $query = 'INSERT INTO users (role_id, username, email, password, hash, created_at, updated_at)
     VALUES(:role_id, :username, :email, :password, :hash, :created_at, :updated_at)';
 
-    // Подготавливаем запрос на выполнение.
     $sth = connectionDB()->prepare($query);
 
-    // Сегодняшняя дата и время.
     $now = \date('Y-m-d H:i:s');
-    // Запускаем подготовленный запрос на выполнения, передавая туда массив значений для именованных параметров.
+
     $sth->execute([
         ':role_id' => '0',
         ':username' => $data['user_name'],
         ':email' => $data['email'],
         ':password' => $data['password'],
-        ':hash' => \password_hash($data['password'], PASSWORD_DEFAULT),
+        ':hash' => '0',
         ':created_at' => $now,
         ':updated_at' => $now,
     ]);
 
-    // Возвращаем айди последней созданной записи.
-    return connectionDB()->lastInsertId();
+    return (int)connectionDB()->lastInsertId();
 }
 
 /** Проверяем идентичны ли введенные пользователем пароли.
- * @param string $password
- * @param string $confirmPassword
- * @return bool
  */
 function isPasswordsMatch(string $password, string $confirmPassword): bool
 {
-    // true, если пароли совпали.
     return $password === $confirmPassword;
 }
